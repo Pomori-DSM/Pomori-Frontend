ed35d6b6aa8c98c8fdee4061e58c0c94fcb37e62
ojinikim <ojinikim@gmail.com>
2024-10-31 23:47:03 +0900

    add :: 회원가입 페이지 퍼블리싱

---
+++ app/(tabs)/Onboarding/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Login() {
+  return <Text>아아아ㅏㅏㅏㅇ</Text>;
+}
---
+++ app/(tabs)/Signup.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+export default function Signup() {
+  return <Text>아시발~</Text>;
+}
