984c9f7ebcc59cad6b86f0239ebbe7c59abb3b71
ojinikim <ojinikim@gmail.com>
2024-10-31 23:42:14 +0900

    add :: Button, TopBar 컴포넌트 추가

---
+++ components/Button.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Txt, color } from "@/styles";
+import styled from "styled-components/native";
+import { TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+
+interface ButtonStyle {
+  backgroundColor: string;
+  textColor: string;
+  borderColor?: string;
+  borderWidth?: number;
+}
+
+interface ButtonProps {
+  text?: string;
+  type?: keyof typeof buttonStyle;
+  onPress?: () => void;
+  keyboardOpen?: boolean;
+}
+
+export const Button = ({
+  text,
+  type = "primary",
+  onPress,
+  keyboardOpen,
+}: ButtonProps) => {
+  const { textColor, ...otherStyles } = buttonStyle[type];
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[otherStyles, styles.box, { borderRadius: keyboardOpen ? 0 : 8 }]}
+    >
+      <Txt textStyle="medium18" style={{ color: textColor }}>
+        {text}
+      </Txt>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    width: "100%",
+    height: 56,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const buttonStyle: { [key: string]: ButtonStyle } = {
+  primary: {
+    backgroundColor: color.gray900,
+    textColor: color.white,
+  },
+  white: {
+    backgroundColor: color.white,
+    textColor: color.black,
+    borderColor: color.gray900,
+    borderWidth: 1,
+  },
+};
---
+++ components/TopBar.tsx
@@ -0,0 +1,38 @@
+import { Blank } from "@/assets";
+import { Txt, color } from "@/styles";
+import React from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+
+interface TopbarProps {
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  text?: string;
+}
+
+export const TopBar = ({
+  leftIcon,
+  text,
+  rightIcon = <Blank />,
+}: TopbarProps) => {
+  return (
+    <View style={styles.container}>
+      {leftIcon}
+      <Txt textStyle="medium18">{text}</Txt>
+      {rightIcon}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get("window").width,
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+    height: 56,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    justifyContent: "space-between",
+    backgroundColor: color.white,
+  },
+});
