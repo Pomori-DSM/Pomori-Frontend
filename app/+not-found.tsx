b03fb792e30e7dedd9f89130da62a482a486b4dc
ojinikim <ojinikim@gmail.com>
2024-11-18 23:40:50 +0900

    edit :: 온보딩 화면

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,27 +1,108 @@
-import React from "react";
-import { Link, Stack } from "expo-router";
-import { StyleSheet, Text } from "react-native";
+import { Txt, color } from "@/styles";
+import React, { useState } from "react";
+import { Dimensions, ScrollView, StyleSheet, View } from "react-native";
+import { Button } from "@/components";
+import { useNavigation } from "expo-router";
+import { NavigationProp } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function NotFoundScreen() {
+export default function Onboarding() {
+  const [imageNumber, setImageNumber] = useState<number>(0);
+  const images = [
+    <View style={styles.image} />,
+    <View style={styles.image} />,
+    <View style={styles.image} />,
+  ];
+  const navigation = useNavigation<NavigationProp<any>>();
+  AsyncStorage.removeItem("accessToken");
+  AsyncStorage.clear();
   return (
-    <>
-      <Stack.Screen options={{ title: "Oops!" }} />
-      <Link href="/" style={styles.link}>
-        <Text>Go to home screen!</Text>
-      </Link>
-    </>
+    <View style={styles.container}>
+      <View style={styles.titleBox}>
+        <Txt textStyle="semibold16" color="gray500">
+          POMORI에 오신 걸 환영해요
+        </Txt>
+        <Txt textStyle="semibold36" color="black">
+          나만의 목표를{"\n"}추가하고 관리해요
+        </Txt>
+      </View>
+      <View style={styles.imageSlideBox}>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          style={styles.imageSlide}
+        >
+          {images.map((img, index) => (
+            <View key={index} style={styles.image} />
+          ))}
+        </ScrollView>
+        <View style={styles.dotIndicatorBox}>
+          {images.map((_, index) => (
+            <View
+              key={index}
+              style={[styles.dot, imageNumber === index && styles.activeDot]}
+            />
+          ))}
+        </View>
+      </View>
+      <View style={styles.buttonBox}>
+        <Button
+          onPress={() => navigation.navigate("Login")}
+          type="primary"
+          text="로그인"
+        />
+        <Button
+          onPress={() => navigation.navigate("(tabs)")}
+          type="white"
+          text="게스트로 시작하기"
+        />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
+    backgroundColor: color.white,
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    gap: 24,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  titleBox: {
+    width: "100%",
+    gap: 12,
+  },
+  imageSlide: {
+    width: "100%",
+  },
+  image: {
+    borderRadius: 16,
+    backgroundColor: color.gray300,
+    width: Dimensions.get("window").width - 40,
+  },
+  imageSlideBox: {
+    flex: 1,
+    gap: 24,
+  },
+  dotIndicatorBox: {
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 8,
+  },
+  dot: {
+    borderRadius: 8,
+    backgroundColor: color.gray200,
+    width: 8,
+    height: 8,
+  },
+  activeDot: {
+    backgroundColor: color.black,
+    width: 40,
+  },
+  buttonBox: {
+    width: "100%",
+    gap: 8,
   },
 });
